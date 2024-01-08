52a8767c3821a8aca4372d23904e1e5b939bdf9a
kaleeswaran-E <kaleeswaran762003@gmail.com>
2024-01-08 12:27:51 +0530

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ToDoList from "./components/ToDoList";
+import "./components/ToDoList.jsx";
+import "./Components/ToDoList.css";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
